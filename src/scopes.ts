40acfbfb7545c6867218f4336695974a1c50adcc
André Posso <admin@lzptec.com>
2024-04-17 22:22:12 -0300

    feat: new Container Scope.

--- src/scopes.ts
+++ src/scopes.ts
@@ -1,5 +1,21 @@
 export enum Scopes {
+    /**
+     * Creates a single instance on the application.
+     */
     SINGLETON = 0,
+
+    /**
+     * Creates a single instance per container.
+     */
     CONTEXT = 1,
-    INSTANCE = 2
+
+    /**
+     * Creates a new instance on every injection.
+     */
+    INSTANCE = 2,
+
+    /**
+     * Creates a single instance on the container.
+     */
+    CONTAINER = 3
 }
